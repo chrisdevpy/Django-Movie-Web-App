24ebab74a323b307a1b9b6c4cd007470fa759da5
chris_dev <yanezchristopher55@gmail.com>
2019-10-12 03:56:30 +0000

    Created Django Movie Web App

--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from airtable import Airtable
 import os
 
-
 AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
               'Movies',
               api_key=os.environ.get('AIRTABLE_API_KEY'))
@@ -20,10 +19,41 @@ def create(request):
     if request.method == 'POST':
         data = {
             'Name': request.POST.get('name'),
-            'Pictures': [{'url': request.POST.get('url')}],
+            'Pictures': [{'url': request.POST.get('url') or 'https://cdn-tp1.mozu.com/21830-33325/resources/images/no-product-image.png?_mzcb=_1565967489995'}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes')
         }
 
-        AT.insert(data)
+        try:
+            response = AT.insert(data)
+            messages.success(request, 'New movie added: {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, 'Got an error when trying to create a new movie: {}'.format(e))
+
+    return redirect('/')
+
+def edit(request, movie_id):
+    if request.method == 'POST':
+        data = {
+            'Name': request.POST.get('name'),
+            'Pictures': [{'url': request.POST.get('url') or 'https://cdn-tp1.mozu.com/21830-33325/resources/images/no-product-image.png?_mzcb=_1565967489995'}],
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes')
+        }
+        try:
+            response = AT.update(movie_id, data)
+            messages.success(request, 'Updated movie: {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, 'Got an error when trying to update movie: {}'.format(e))
+
+    return redirect('/')
+
+def delete(request, movie_id):
+    try:
+        movie_name = AT.get(movie_id)['fields'].get('Name')
+        response = AT.delete(movie_id)
+        messages.warning(request, 'Deleted movie: {}'.format(movie_name))
+    except Exception as e:
+        messages.warning(request, 'Got an error when trying to delete a movie: {}'.format(e))
+
     return redirect('/')
